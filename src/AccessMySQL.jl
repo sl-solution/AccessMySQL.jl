8d281e60aa3cff5f6deef15debc47a3a30944e64
sl-solution <github.sl.solution@icloud.com>
2023-03-12 21:46:55 +1300

    Update AccessMySQL.jl

--- src/AccessMySQL.jl
+++ src/AccessMySQL.jl
@@ -1,2 +1,11 @@
+"""
+    AccessMySQL
+
+Provide efficient APIs for fetching and loading data from/into MySQL using MySQL C API.
+"""
 module AccessMySQL
+
+using InMemoryDatasets
+using MySQL
+
 end
